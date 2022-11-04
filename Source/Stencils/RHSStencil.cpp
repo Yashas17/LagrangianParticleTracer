afa780508e85189a4f6febdee556e0d8fd36afc6
Yashas <y.gandhi@tum.de>
2022-11-04 17:06:20 +0100

    Fixed RHSStencil

--- Source/Stencils/RHSStencil.cpp
+++ Source/Stencils/RHSStencil.cpp
@@ -10,10 +10,10 @@ void Stencils::RHSStencil::apply(FlowField& flowField, int i, int j) {
   const RealType dx = 0.5 * (parameters_.meshsize->getDx(i, j) + parameters_.meshsize->getDx(i - 1, j));
   const RealType dy = 0.5 * (parameters_.meshsize->getDy(i, j) + parameters_.meshsize->getDy(i, j - 1));
 
-  VectorField& velocity = flowField.getVelocity();
+  VectorField& FGH = flowField.getFGH();
 
-  RealType dudx = (velocity.getVector(i, j)[0] - velocity.getVector(i - 1, j)[0]) / dx;
-  RealType dvdy = (velocity.getVector(i, j)[1] - velocity.getVector(i, j - 1)[1]) / dy;
+  RealType dudx = (FGH.getVector(i, j)[0] - FGH.getVector(i - 1, j)[0]) / dx;
+  RealType dvdy = (FGH.getVector(i, j)[1] - FGH.getVector(i, j - 1)[1]) / dy;
 
   flowField.getRHS().getScalar(i, j) = (dudx + dvdy) / dt;
 }
@@ -24,11 +24,11 @@ void Stencils::RHSStencil::apply(FlowField& flowField, int i, int j, int k) {
   const RealType dy = 0.5 * (parameters_.meshsize->getDy(i, j, k) + parameters_.meshsize->getDy(i, j - 1, k));
   const RealType dz = 0.5 * (parameters_.meshsize->getDz(i, j, k) + parameters_.meshsize->getDz(i, j, k - 1));
 
-  VectorField& velocity = flowField.getVelocity();
+  VectorField& FGH = flowField.getFGH();
 
-  RealType dudx = (velocity.getVector(i, j, k)[0] - velocity.getVector(i - 1, j, k)[0]) / dx;
-  RealType dvdy = (velocity.getVector(i, j, k)[1] - velocity.getVector(i, j - 1, k)[1]) / dy;
-  RealType dwdz = (velocity.getVector(i, j, k)[2] - velocity.getVector(i, j, k - 1)[2]) / dz;
+  RealType dudx = (FGH.getVector(i, j, k)[0] - FGH.getVector(i - 1, j, k)[0]) / dx;
+  RealType dvdy = (FGH.getVector(i, j, k)[1] - FGH.getVector(i, j - 1, k)[1]) / dy;
+  RealType dwdz = (FGH.getVector(i, j, k)[2] - FGH.getVector(i, j, k - 1)[2]) / dz;
 
   flowField.getRHS().getScalar(i, j, k) = (dudx + dvdy + dwdz) / dt;
 }
