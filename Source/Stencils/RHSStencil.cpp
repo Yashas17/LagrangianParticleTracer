eb72e0b5302d6240a0b6e7fa27ce7188dcea9b19
Yashas <y.gandhi@tum.de>
2022-11-05 11:33:10 +0100

    Fixed dx, dy and dz calculation

--- Source/Stencils/RHSStencil.cpp
+++ Source/Stencils/RHSStencil.cpp
@@ -7,28 +7,23 @@ Stencils::RHSStencil::RHSStencil(const Parameters& parameters):
 
 void Stencils::RHSStencil::apply(FlowField& flowField, int i, int j) {
   const RealType dt = parameters_.timestep.dt;
-  const RealType dx = 0.5 * (parameters_.meshsize->getDx(i, j) + parameters_.meshsize->getDx(i - 1, j));
-  const RealType dy = 0.5 * (parameters_.meshsize->getDy(i, j) + parameters_.meshsize->getDy(i, j - 1));
 
   VectorField& FGH = flowField.getFGH();
 
-  RealType dudx = (FGH.getVector(i, j)[0] - FGH.getVector(i - 1, j)[0]) / dx;
-  RealType dvdy = (FGH.getVector(i, j)[1] - FGH.getVector(i, j - 1)[1]) / dy;
+  RealType dudx = (FGH.getVector(i, j)[0] - FGH.getVector(i - 1, j)[0]) / parameters_.meshsize->getDx(i, j);
+  RealType dvdy = (FGH.getVector(i, j)[1] - FGH.getVector(i, j - 1)[1]) / parameters_.meshsize->getDy(i, j);
 
   flowField.getRHS().getScalar(i, j) = (dudx + dvdy) / dt;
 }
 
 void Stencils::RHSStencil::apply(FlowField& flowField, int i, int j, int k) {
   const RealType dt = parameters_.timestep.dt;
-  const RealType dx = 0.5 * (parameters_.meshsize->getDx(i, j, k) + parameters_.meshsize->getDx(i - 1, j, k));
-  const RealType dy = 0.5 * (parameters_.meshsize->getDy(i, j, k) + parameters_.meshsize->getDy(i, j - 1, k));
-  const RealType dz = 0.5 * (parameters_.meshsize->getDz(i, j, k) + parameters_.meshsize->getDz(i, j, k - 1));
 
   VectorField& FGH = flowField.getFGH();
 
-  RealType dudx = (FGH.getVector(i, j, k)[0] - FGH.getVector(i - 1, j, k)[0]) / dx;
-  RealType dvdy = (FGH.getVector(i, j, k)[1] - FGH.getVector(i, j - 1, k)[1]) / dy;
-  RealType dwdz = (FGH.getVector(i, j, k)[2] - FGH.getVector(i, j, k - 1)[2]) / dz;
+  RealType dudx = (FGH.getVector(i, j, k)[0] - FGH.getVector(i - 1, j, k)[0]) / parameters_.meshsize->getDx(i, j, k);
+  RealType dvdy = (FGH.getVector(i, j, k)[1] - FGH.getVector(i, j - 1, k)[1]) / parameters_.meshsize->getDy(i, j, k);
+  RealType dwdz = (FGH.getVector(i, j, k)[2] - FGH.getVector(i, j, k - 1)[2]) / parameters_.meshsize->getDz(i, j, k);
 
   flowField.getRHS().getScalar(i, j, k) = (dudx + dvdy + dwdz) / dt;
 }
