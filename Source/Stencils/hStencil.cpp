1f9078a4ca2b17743a3d4aeb54acf7b1981e30e6
Y_Shehata <y.shehata@tum.de>
2022-12-10 20:45:34 +0100

    fixed h stencil

--- Source/Stencils/hStencil.cpp
+++ Source/Stencils/hStencil.cpp
@@ -2,7 +2,6 @@
 
 #include "hStencil.hpp"
 
-#include <algorithm>
 
 Stencils::hStencil::hStencil(const Parameters& parameters):
   FieldStencil<FlowField>(parameters),
@@ -10,20 +9,72 @@ Stencils::hStencil::hStencil(const Parameters& parameters):
   yLimit_(parameters.bfStep.yRatio * parameters.geometry.lengthY) {}
 
 void Stencils::hStencil::apply(FlowField& flowField, int i, int j) {
-  RealType value = flowField.getH().getScalar(i, j);
+  RealType h = flowField.getH().getScalar(i, j);
 
   const RealType posX = parameters_.meshsize->getPosX(i, j);
   const RealType posY = parameters_.meshsize->getPosY(i, j);
 
-  value = std::min(posX, posY);
+  const RealType lengthX = parameters_.geometry.lengthX;
+  const RealType lengthY = parameters_.geometry.lengthY;
+
+  const RealType stepLengthX = parameters_.bfStep.xRatio * lengthX;
+  const RealType stepLengthY = parameters_.bfStep.yRatio * lengthY;
+
+  // Check if this is the BFS case
+  if(parameters_.bfStep.xRatio > 0 && parameters_.bfStep.yRatio > 0) {
+    // Case 1: the step is a left wall to the current cell
+    if(posY <= stepLengthY && (posX - stepLengthX) > 0)
+      h = std::min(posX - stepLengthX, std::min(posY, lengthY - posY));
+
+    // Case 2: the cell is above the step
+    else if(posX - stepLengthX < 0)
+      h = std::min(posY - stepLengthY, lengthY - (posY - stepLengthY));
+
+    // Case 3: the cell is above the level of the step but not above the step itself
+    else
+      h = std::min(posY, lengthY - posY);
+
+  }
+  else {
+    // channel flow case
+    h = std::min(lengthY - posY, posY);
+  }
 }
 
 void Stencils::hStencil::apply(FlowField& flowField, int i, int j, int k) {
-  RealType value = flowField.getH().getScalar(i, j, k);
+  RealType h = flowField.getH().getScalar(i, j, k);
 
   const RealType posX = parameters_.meshsize->getPosX(i, j, k);
   const RealType posY = parameters_.meshsize->getPosY(i, j, k);
   const RealType posZ = parameters_.meshsize->getPosZ(i, j, k);
 
-  value = std::min(posX, std::min(posY, posZ));
+  const RealType lengthX = parameters_.geometry.lengthX;
+  const RealType lengthY = parameters_.geometry.lengthY;
+  const RealType lengthZ = parameters_.geometry.lengthZ;
+
+  const RealType stepLengthX = parameters_.bfStep.xRatio * lengthX;
+  const RealType stepLengthY = parameters_.bfStep.yRatio * lengthY;
+
+  // Check if this is the BFS case
+  if(parameters_.bfStep.xRatio > 0 && parameters_.bfStep.yRatio > 0) {
+    // Case 1: the step is a left wall to the current cell
+    if(posY <= stepLengthY && (posX - stepLengthX) > 0)
+      h = std::min(posX - stepLengthX, std::min(posY, lengthY - posY));
+
+    // Case 2: the cell is above the step
+    else if(posX - stepLengthX < 0)
+      h = std::min(posY - stepLengthY, lengthY - (posY - stepLengthY));
+
+    // Case 3: the cell is above the level of the step but not above the step itself
+    else
+      h = std::min(posY, lengthY - posY);
+
+  }
+  else {
+    // channel flow case
+    h = std::min(lengthY - posY, posY);
+  }
+
+  // Considering the distance from the front and back walls (z direction) 
+  h = std::min(h, std::min(posZ, lengthZ - posZ));
 }
