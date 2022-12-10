0009bde26d2748e8428f191351398e3a8face11e
Yashas <y.gandhi@tum.de>
2022-12-10 15:07:51 +0100

    Fixed data type

--- Source/Stencils/TimeStepStencil.hpp
+++ Source/Stencils/TimeStepStencil.hpp
@@ -9,11 +9,11 @@ namespace Stencils {
   /**Stencil to compute the time step for the turbulent simulation.
    */
   class TimeStepStencil: public FieldStencil<FlowField> {
-    public:
-        TimeStepStencil(const Parameters& parameters);
-        ~TimeStepStencil() override = default;
+  public:
+    TimeStepStencil(const Parameters& parameters);
+    ~TimeStepStencil() override = default;
 
-        void apply(FlowField& flowField, RealType& dt, int i, int j) override;
-        void apply(FlowField& flowField, RealType& dt, int i, int j, int k) override;
+    void apply(FlowField& flowField, int i, int j) override;
+    void apply(FlowField& flowField, int i, int j, int k) override;
   };
 } // namespace Stencils
