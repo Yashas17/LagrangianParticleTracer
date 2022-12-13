1fd28bb9d71ab6aabb8543c3dfb99c6504078e02
Y_Shehata <y.shehata@tum.de>
2022-12-13 01:50:21 +0100

    considered distance from corner for hstencil

--- Source/Stencils/hStencil.cpp
+++ Source/Stencils/hStencil.cpp
@@ -10,15 +10,17 @@ Stencils::hStencil::hStencil(const Parameters& parameters):
 
 void Stencils::hStencil::apply(FlowField& flowField, int i, int j) {
   RealType& h = flowField.getH().getScalar(i, j);
+  const RealType dx2 = parameters_.meshsize->getDx(i,j)/2;
+  const RealType dy2 = parameters_.meshsize->getDy(i,j)/2;
 
-  const RealType posX = parameters_.meshsize->getPosX(i, j) + parameters_.meshsize->getDx(i,j)/2;
-  const RealType posY = parameters_.meshsize->getPosY(i, j) + parameters_.meshsize->getDy(i,j)/2;
+  const RealType posX = parameters_.meshsize->getPosX(i, j) + dx2;
+  const RealType posY = parameters_.meshsize->getPosY(i, j) + dy2;
 
   const RealType lengthX = parameters_.geometry.lengthX;
   const RealType lengthY = parameters_.geometry.lengthY;
 
-  const RealType stepLengthX = parameters_.bfStep.xRatio * lengthX;
-  const RealType stepLengthY = parameters_.bfStep.yRatio * lengthY;
+  const RealType stepLengthX = parameters_.bfStep.xRatio * lengthX + dx2;
+  const RealType stepLengthY = parameters_.bfStep.yRatio * lengthY + dy2;
 
   // Check if this is the BFS case
   if(parameters_.bfStep.xRatio > 0 && parameters_.bfStep.yRatio > 0) {
@@ -31,9 +33,14 @@ void Stencils::hStencil::apply(FlowField& flowField, int i, int j) {
       h = std::min(posY - stepLengthY, lengthY - posY);
     }
 
-    // Case 3: the cell is above the level of the step but not above the step itself
-    else
-      h = std::min(posY, lengthY - posY);
+    // Case 3: the cell is above the level of the step but not above the step
+    else {
+      const RealType distanceToStepX = posX - stepLengthX;
+      const RealType distanceToStepY = posY - stepLengthY;
+      const RealType distanceToStepCorner = std::hypot(distanceToStepX, distanceToStepY);
+
+      h = std::min(distanceToStepCorner , std::min(posY, lengthY - posY));
+    }
 
   }
   else {
@@ -44,17 +51,20 @@ void Stencils::hStencil::apply(FlowField& flowField, int i, int j) {
 
 void Stencils::hStencil::apply(FlowField& flowField, int i, int j, int k) {
   RealType& h = flowField.getH().getScalar(i, j, k);
+  const RealType dx2 = parameters_.meshsize->getDx(i,j,k)/2;
+  const RealType dy2 = parameters_.meshsize->getDy(i,j,k)/2;
+  const RealType dz2 = parameters_.meshsize->getDz(i,j,k)/2;
 
-  const RealType posX = parameters_.meshsize->getPosX(i, j, k) + parameters_.meshsize->getDx(i,j,k)/2;
-  const RealType posY = parameters_.meshsize->getPosY(i, j, k) + parameters_.meshsize->getDy(i,j,k)/2;
-  const RealType posZ = parameters_.meshsize->getPosZ(i, j, k) + parameters_.meshsize->getDz(i,j,k)/2;
+  const RealType posX = parameters_.meshsize->getPosX(i, j, k) + dx2;
+  const RealType posY = parameters_.meshsize->getPosY(i, j, k) + dy2;
+  const RealType posZ = parameters_.meshsize->getPosZ(i, j, k) + dz2;
 
   const RealType lengthX = parameters_.geometry.lengthX;
   const RealType lengthY = parameters_.geometry.lengthY;
   const RealType lengthZ = parameters_.geometry.lengthZ;
 
-  const RealType stepLengthX = parameters_.bfStep.xRatio * lengthX;
-  const RealType stepLengthY = parameters_.bfStep.yRatio * lengthY;
+  const RealType stepLengthX = parameters_.bfStep.xRatio * lengthX + dx2;
+  const RealType stepLengthY = parameters_.bfStep.yRatio * lengthY + dy2;
 
   // Check if this is the BFS case
   if(parameters_.bfStep.xRatio > 0 && parameters_.bfStep.yRatio > 0) {
@@ -67,8 +77,13 @@ void Stencils::hStencil::apply(FlowField& flowField, int i, int j, int k) {
       h = std::min(posY - stepLengthY, lengthY - posY);
 
     // Case 3: the cell is above the level of the step but not above the step itself
-    else
-      h = std::min(posY, lengthY - posY);
+    else {
+      const RealType distanceToStepX = posX - stepLengthX;
+      const RealType distanceToStepY = posY - stepLengthY;
+      const RealType distanceToStepCorner = std::hypot(distanceToStepX, distanceToStepY);
+
+      h = std::min(distanceToStepCorner , std::min(posY, lengthY - posY));
+    }
 
   }
   else {
